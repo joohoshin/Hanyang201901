eb2c9e0af18f7c2ba66c406a632306201f6f7e1a
jooho_office <jooho@boozaapp.com>
2019-05-01 15:12:27 +0900

    tangent plot add

--- Class7_Numpy.py
+++ Class7_Numpy.py
@@ -66,3 +66,24 @@ plt.plot(x,y)
 x2 = np.arange(20,40, 0.2)
 y2 = np.sin(x2)
 plt.plot(x2,y2)
+
+
+### 탄젠트 그리기
+# import library
+import matplotlib.pyplot as plt
+import numpy as np
+
+# x,y 값을 지정
+x = np.arange(-10.0, 10.0, 0.001)
+y = np.tan(x)
+
+# tangent는 무한대 값이 있으므로 그리기 위해서
+# 출력할 y값의 한도를 정한다. 
+ylim = 10
+
+# 출력할 y값보다 큰 값을 nan을 넣어준다. (데이터 삭제)
+y[y > ylim] = np.nan
+y[y < -ylim] = np.nan
+
+# 출력을 한다. 
+plt.plot(x, y)
